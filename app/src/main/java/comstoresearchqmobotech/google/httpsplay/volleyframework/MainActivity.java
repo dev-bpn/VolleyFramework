1b31ba560cd5a673e74e2658dc416668ed69790d
dev-bpn <jazzbpn@gmail.com>
2015-11-21 18:44:29 +0545

    StringRequest using SINGLETON PATTERN

--- app/src/main/java/comstoresearchqmobotech/google/httpsplay/volleyframework/MainActivity.java
+++ app/src/main/java/comstoresearchqmobotech/google/httpsplay/volleyframework/MainActivity.java
@@ -6,19 +6,62 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import comstoresearchqmobotech.google.httpsplay.volleyframework.my_volley.MySingleton;
+import comstoresearchqmobotech.google.httpsplay.volleyframework.utils.MyUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     private String url = "https://www.google.com";
     private TextView textView;
+    private RequestQueue requestQueue;
+    private final String TAG = "MY_TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);
+        myStringRequestUsingSingleton();
 
     }
-    
+
+    private void myStringRequestUsingSingleton(){
+
+        if(MyUtils.isNetworkConnected(this)){
+
+            requestQueue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    textView.setText("Response: " + response.substring(0 , 200));
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    textView.setText("Error: "+ error.toString());
+                }
+            });
+            stringRequest.setTag(TAG);
+            MySingleton.getInstance(this).addToRequestQueue(stringRequest);
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(requestQueue != null){
+            requestQueue.cancelAll(TAG);
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
