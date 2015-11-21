f6776b18c2cda1d2631e9514fe034ee4a2fa2243
dev-bpn <jazzbpn@gmail.com>
2015-11-21 18:16:49 +0545

    Network and Cache using StringRequest

--- app/src/main/java/comstoresearchqmobotech/google/httpsplay/volleyframework/MainActivity.java
+++ app/src/main/java/comstoresearchqmobotech/google/httpsplay/volleyframework/MainActivity.java
@@ -6,19 +6,67 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+
+import comstoresearchqmobotech.google.httpsplay.volleyframework.utils.MyUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     private String url = "https://www.google.com";
     private TextView textView;
+    private RequestQueue requestQueue;
+    private static final String TAG = "MY_TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);
+        myRequestQueue();
 
     }
 
+    private void myRequestQueue(){
+
+        if(MyUtils.isNetworkConnected(this)){
+
+            Cache cache = new DiskBasedCache(getCacheDir() , 1024*1024);
+            Network network = new BasicNetwork(new HurlStack());
+            requestQueue = new RequestQueue(cache , network);
+            requestQueue.start();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    textView.setText("Response: "+ response.substring(0 , 200));
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    textView.setText("Error: "+error.toString());
+                }
+            });
+            requestQueue.add(stringRequest);
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(requestQueue != null){
+            requestQueue.cancelAll(TAG);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
