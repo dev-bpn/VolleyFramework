876bb5038ec6a0b3d1dde7338b86f1aa6ca5e7a5
dev-bpn <jazzbpn@gmail.com>
2015-11-21 21:47:24 +0545

    JsonObjectRequest with Singleton

--- app/src/main/java/comstoresearchqmobotech/google/httpsplay/volleyframework/MainActivity.java
+++ app/src/main/java/comstoresearchqmobotech/google/httpsplay/volleyframework/MainActivity.java
@@ -4,26 +4,50 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.TextView;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-
 import org.json.JSONObject;
-
 import comstoresearchqmobotech.google.httpsplay.volleyframework.my_volley.MySingleton;
+import comstoresearchqmobotech.google.httpsplay.volleyframework.utils.MyUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String URL = "http://api.androidhive.info/volley/person_object.json";
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
 
+        myJsonObjectRequest();
 
+    }
 
+    private void myJsonObjectRequest() {
+
+        if(MyUtils.isNetworkConnected(this)){
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            textView.setText("Response: "+ response);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    textView.setText("Error: "+ error.toString());
+                }
+            });
+
+            MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+
+        }
     }
 
 
