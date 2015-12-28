f28d3f5fad90549771defab8b3a4bc03bff77425
dev-bpn <jazzbpn@gmail.com>
2015-12-28 11:21:02 +0545

    MkeUp

--- app/src/main/java/comstoresearchqmobotech/google/httpsplay/volleyframework/MainActivity.java
+++ app/src/main/java/comstoresearchqmobotech/google/httpsplay/volleyframework/MainActivity.java
@@ -4,53 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-
-import org.json.JSONArray;
-
-import comstoresearchqmobotech.google.httpsplay.volleyframework.my_volley.MySingleton;
-import comstoresearchqmobotech.google.httpsplay.volleyframework.utils.MyUtils;
 
 // CHECK THIS LINK
 // http://www.androidhive.info/2014/05/android-working-with-volley-library-1/
+// http://www.androidhive.info/2014/07/android-custom-listview-with-image-and-text-using-volley/
+// Custom ListView using Volley
 
 public class MainActivity extends AppCompatActivity {
-
-    private final String URL = "http://api.androidhive.info/volley/person_array.json";
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.textView);
 
-        myJsonArrayRequest();
 
-    }
-
-    private void myJsonArrayRequest() {
-
-        if(MyUtils.isNetworkConnected(this)){
-
-            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, URL, new Response.Listener<JSONArray>() {
-                @Override
-                public void onResponse(JSONArray response) {
-                    textView.setText("Response: " + response);
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    textView.setText("Error: "+ error);
-                }
-            });
-            MySingleton.getInstance(this).addToRequestQueue(jsonArrayRequest);
-        }
     }
 
 
